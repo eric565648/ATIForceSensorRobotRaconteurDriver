7286e7c2e88bb361eb8ccbf8ab1eae4a7aa00fbd
Eric Lu <eric565648@gmail.com>
2021-10-20 18:57:10 -0400

    read service

--- robotraconteur_ati_driver.py
+++ robotraconteur_ati_driver.py
@@ -14,7 +14,7 @@ class ATIDriver(object):
 
         self._lock = threading.RLock()
         self._recv_lock=threading.RLock()
-        self._steaming = False
+        self._streaming = False
         
         self.host = host
         self.ati_obj = rpi_ati_net_ft.NET_FT(host)
@@ -38,24 +38,28 @@ class ATIDriver(object):
 
     def stream_loop(self):
         
-        while self._steaming:
+        while self._streaming:
             if(not self._streaming): return
             res, ft, status = self.ati_obj.try_read_ft_streaming(.1)
             self.send_sensor_val(ft)
 
     def send_sensor_val(self, ft):
         
-        # create the new structure
-        strt = RRN.NewStructure('com.robotraconteur.geometry.Wrench')
+        # create the new namedarray
+        arraytype = RRN.GetNamedArrayDType('com.robotraconteur.geometry.Wrench')
+        # print(arraytype)
+        msg_arr = np.zeros((1,),dtype=arraytype)
+        # print(msg_arr)
         # pack the data to the structure to send to the client
-        strt.torque.x = ft[0]
-        strt.torque.y = ft[1]
-        strt.torque.z = ft[2]
-        strt.force.x = ft[3]
-        strt.force.y = ft[4]
-        strt.force.z = ft[5]
+        msg_arr[0]['torque']['x'] = ft[0]
+        msg_arr[0]['torque']['y'] = ft[1]
+        msg_arr[0]['torque']['z'] = ft[2]
+        msg_arr[0]['force']['x'] = ft[2]
+        msg_arr[0]['force']['y'] = ft[2]
+        msg_arr[0]['force']['z'] = ft[2]
+        # print(msg_arr)
 
-        self.wrench_sensor_value.Outvalue=strt
+        self.wrench_sensor_value.OutValue=msg_arr
 
 def main():
     parser = argparse.ArgumentParser(description="ATI force torque sensor driver service for Robot Raconteur")
@@ -66,10 +70,11 @@ def main():
 
     # not yet know what this do
     rr_args = ["--robotraconteur-jumbo-message=true"] + sys.argv
+    RRC.RegisterStdRobDefServiceTypes(RRN)
 
     ati_obj = ATIDriver(args.sensor_ip)
 
-    with RR.ServerNodeSetup("com.robotraconteur.sensor.WrenchSensor",59823,argv=rr_args):
+    with RR.ServerNodeSetup("com.robotraconteur.sensor.wrenchsensor",59823,argv=rr_args):
         
         service_ctx = RRN.RegisterService("ati_sensor","com.robotraconteur.sensor.WrenchSensor",ati_obj)
         ati_obj.srv_start_streaming()
--- test_ati_client.py
+++ test_ati_client.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
 
+    # url='rr+local:///?nodeid=8570e7c7-b75e-4234-ae69-185caf0c59df&service=ati_sensor'
     url='rr+tcp://localhost:59823?service=ati_sensor'
     if (len(sys.argv)>=2):
         url=sys.argv[1]
@@ -19,19 +20,25 @@ def main():
     #Add callback for when the wire value change
     wrench_wire.WireValueChanged += wrench_wire_cb
 
+    if (sys.version_info > (3, 0)):
+        input("Server started, press enter to quit...")
+    else:
+        raw_input("Server started, press enter to quit...")
+
 
 def wrench_wire_cb(w,value,time):
-
-    val = w.Invalue
+    
 
     print("==============")
-    print("torque.x",val.torque.x)
-    print("torque.y",val.torque.y)
-    print("torque.z",val.torque.z)
-    print("force.x",val.force.x)
-    print("force.y",val.force.y)
-    print("force.z",val.force.z)
+    print("torque.x",value['torque']['x'])
+    print("torque.y",value['torque']['y'])
+    print("torque.z",value['torque']['z'])
+    print("force.x",value['force']['x'])
+    print("force.y",value['force']['y'])
+    print("force.z",value['force']['z'])
     print("==============")
+
+    
 
 if __name__=='__main__':
     main()
